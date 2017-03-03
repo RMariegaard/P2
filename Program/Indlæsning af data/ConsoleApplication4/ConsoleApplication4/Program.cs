3b33742b5300d8343d9e83d9736ea283931684ab
lsvenn <lsvenn16@student.aau.dk>
2017-03-03 19:13:26 +0100

    Oversættelse

--- Program/Indlæsning af data/ConsoleApplication4/ConsoleApplication4/Program.cs
+++ Program/Indlæsning af data/ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -11,24 +11,24 @@ namespace Recommender
         static void Main(string[] args)
         {
             // Fil Indlæsning:
-            string[] fil = System.IO.File.ReadAllLines(@"C:\Users\Lasse\Documents\GitHub\P2\Program\Indlæsning af data\ConsoleApplication8\ConsoleApplication8\user_artists.dat");
-            string[] kunst_fil = System.IO.File.ReadAllLines(@"C:\Users\Lasse\Documents\GitHub\P2\Program\Indlæsning af data\ConsoleApplication8\ConsoleApplication8\artists.dat");
+            string[] file = System.IO.File.ReadAllLines(@"C:\Users\Lasse\Documents\GitHub\P2\Program\Indlæsning af data\ConsoleApplication8\ConsoleApplication8\user_artists.dat");
+            string[] artist_file = System.IO.File.ReadAllLines(@"C:\Users\Lasse\Documents\GitHub\P2\Program\Indlæsning af data\ConsoleApplication8\ConsoleApplication8\artists.dat");
             string[] tag_fil = System.IO.File.ReadAllLines(@"C:\Users\Lasse\Documents\GitHub\P2\Program\Indlæsning af data\ConsoleApplication8\ConsoleApplication8\user_taggedartists.dat");
 
             // Initialisering af User list og Kunstner Arrayet:
             List<User> Users = new List<User>();
-            Artist[] kunstere = new Artist[17632];
+            Artist[] Artists = new Artist[17632];
 
-            // "kunstnere" array udfyldes med alle kunstnere i datasættet:
+            // "Artists" array udfyldes med alle kunstnere i datasættet:
             int i = 0;
-            foreach (var streng in kunst_fil.Skip(1))
+            foreach (var String in artist_file.Skip(1))
             {
-                string[] data = streng.Split('\t');
-                Artist ny = new Artist(int.Parse(data[0]), data[1]);
+                string[] data = String.Split('\t');
+                Artist New = new Artist(int.Parse(data[0]), data[1]);
 
-                ny.Id = int.Parse(data[0]);
-                ny.Name = data[1];
-                kunstere.SetValue(ny, i);
+                New.Id = int.Parse(data[0]);
+                New.Name = data[1];
+                Artists.SetValue(New, i);
                 i++;
             }
 
@@ -36,12 +36,12 @@ namespace Recommender
             // 0 = bruger ID, 1 = Artist Id, 2 = weight
             int prev = 0;
             int index = -1;
-            var fil1 = fil.Skip(1);
+            var file1 = file.Skip(1);
 
-            foreach (var linje in fil1)
+            foreach (var line in file1)
             {
                 // Filen opsplittes:
-                string[] data = linje.Split('\t');  
+                string[] data = line.Split('\t');  
                
                 // Hvis ikke User ID allerede eksisteres laves der en ny User:
                 if (prev != int.Parse(data[0]))
@@ -60,6 +60,8 @@ namespace Recommender
 
             }
 
+            // Tags bliver overført til respektive kunstnere:
+            foreach (Artist )
 
 
 
