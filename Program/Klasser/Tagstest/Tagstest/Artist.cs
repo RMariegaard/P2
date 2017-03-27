616a172d795bcdb687d41674131d8fefc0a15823
lsvenn <lsvenn16@student.aau.dk>
2017-03-27 12:47:55 +0200

    Klasser
    Et udkast til klasser

---
+++ Program/Klasser/Tagstest/Tagstest/Artist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagstest
+{
+    class Artist
+    {
+        public string _name;
+        public Tags[] artisttags;
+
+        private bool done = false;
+        int i = 0;
+
+        public Artist(int[] TagID, int[] TagWeight, string Name) // Denne constructer skal laves om så den læser fra binær fil.
+        {
+            _name = Name;
+
+            while (!done)
+            {
+                artisttags[i] = new Tags(TagID[i], TagWeight[i]);
+                i++;
+                if(i == TagID.Length)
+                {
+                    done = true;
+                }
+            }
+        }
+    }
+}
---
+++ Program/Klasser/Tagstest/Tagstest/Tags.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tagstest
+{
+    class Tags
+    {
+        public int _ID;
+        public int _Weight;
+
+        public Tags(int ID, int Weight)
+        {
+            _ID = ID;
+            _Weight = Weight;
+        }
+    }
+}
