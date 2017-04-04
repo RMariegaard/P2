516dfedd9dce652a41eda299b0b8920386ad9cf1
casper-lp <cpede16@student.aau.dk>
2017-04-04 18:05:07 +0200

    Pearson På brugeres Tags og Demo med dette i main

--- Program/Contentbased stuff/ConsoleApplication4/PearsonCor.cs
+++ Program/Contentbased stuff/ConsoleApplication4/PearsonCor.cs
@@ -38,5 +38,33 @@ namespace Recommender
         }
 
 
+        public double CalculateUser(User A1, User A2) //Baseret på Tags og ikke Artists
+        {
+
+            double A1Mean = 0.0;
+            A1.Tags.ForEach(tag => A1Mean += tag.weight);
+            A1Mean /= A1.total_tag_amount;
+
+            double A2Mean = 0.0;
+            A2.Tags.ForEach(tag => A2Mean += tag.weight);
+            A2Mean /= A2.total_tag_amount;
+
+            double Top = 0.0;
+
+            foreach (var tagA1 in A1.Tags)
+            {
+                if (A2.Tags.Exists(tag => tag.Id == tagA1.Id))
+                {
+                    Top += (tagA1.weight - A1Mean) * (A2.Tags.Find(tag => tag.Id == tagA1.Id).weight - A2Mean);
+                }
+            }
+            double Buttom = Math.Sqrt(Top);
+
+
+
+            return Top / Buttom;
+        }
+
+
     }
 }
--- Program/Contentbased stuff/ConsoleApplication4/Program.cs
+++ Program/Contentbased stuff/ConsoleApplication4/Program.cs
@@ -23,96 +23,165 @@ namespace Recommender
 
             var cos = new Cosine();
             var pear = new PearsonCor();
-            while (true) { 
-            double prevCos = 0;
-            Artist hej = null;
-            Console.WriteLine(";");
-            int h = int.Parse(Console.ReadLine());
 
-            Artist jeh = Artists.ToList().Find(a => a.Value.Id == h).Value;
-                Dictionary<int, double> rec = new Dictionary<int, double>(); 
+
+
+            //Collaborative med tags
+
             double temp = 0;
-            foreach (var art in Artists)
+            var newUser = Users[550];
+            var sim = new Dictionary<int, double>();
+
+
+            Console.WriteLine("New Users Artists: ");
+            for (int i = 0; i < 10; i++)
             {
-                temp = pear.Calculate(jeh, art.Value);
-                    if (art.Value.Id == jeh.Id)
-                    {
-                        //
-                    }
-                    else
-                    {
-                        rec.Add(art.Value.Id, temp);
-                    }
-
-
+                Console.WriteLine(newUser.Artists.Skip(i).First().ThisArtist.Name);
             }
-                var nname = new ConvertIdName();
-            Console.WriteLine(pear.Calculate(jeh, jeh));
-            Console.WriteLine(jeh.Name);
-                Console.WriteLine( );
-               var list = rec.OrderByDescending(art => art.Value);
-
-                for (int i = 0; i < 5; i++)
-                {
-                    Console.WriteLine(nname.ArtistFromID((list.Skip(i).First().Key), Artists).Name + " " + list.Skip(i).First().Value);
-                }
 
 
-            }
-            
-/*
-            //Variables
-            KeyWordSearch Search = new KeyWordSearch();
-            ConvertIdName Convert = new ConvertIdName();
-            List<Artist> DataArtist = Artists.ToList<Artist>();
-            List<Tag> test = new List<Tag>();
-            Artist BaseArtist = new Artist(0, "Default");
-            BestMatch SingleBest = new BestMatch();
-            double ProcentMatch = 0.0;
-            Artist BestMatch = new Artist(0, "Default");
-            
-            while (true)
+
+                foreach (var userr in Users)
             {
-                Console.Clear();
-                //UserInput
-                UserInput User = new UserInput();
-                Console.Write("Enter An Artist Name or ID: ");
-                while (!User.TryGetArtistFromUserInput(Console.ReadLine(), DataArtist, out BaseArtist))
+                temp = pear.CalculateUser(newUser, userr.Value);
+                if (userr.Value.Id == newUser.Id)
                 {
-                    Console.WriteLine("Cant Find The Artist You Are Looking For");
-                    Console.Write("Enter An Artist Name or ID: ");
-                }
-                if (!BaseArtist.TagIds.Any())
-                {
-                    Console.WriteLine("This artist has no tag");
+                    //
                 }
                 else
                 {
-                    //Finding the best match
-                    FindBestMatch Match = new FindBestMatch();
-                    BestMatch = Match.BestMatch(DataArtist, BaseArtist, out ProcentMatch);
-                    Console.Clear();
+                    sim.Add(userr.Value.Id, temp);
+                }
 
-                    //Printing the result:
-                    Console.WriteLine($"Found The Best Match For: {BaseArtist.Id}: {BaseArtist.Name}");
-                    Console.WriteLine($"The best match is: {BestMatch.Id}: {BestMatch.Name}");
-                    Console.WriteLine($"The Best Match Has {ProcentMatch:0.00}% Tags In Commen With Base");
-                    Console.WriteLine("\n\nThe Commen Tags Are:");
-                    foreach (Tag item in Search.CompareTwoArtists(BestMatch, BaseArtist))
+
+            }
+            
+            var list = sim.OrderByDescending(art => art.Value);
+
+
+            var listeMedNewUSerArtists = new List<Artist>();
+
+            newUser.Artists.ForEach(x => listeMedNewUSerArtists.Add(x.ThisArtist));
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                var tempUser = Users[list.Skip(i).First().Key];
+                Console.WriteLine(sim[tempUser.Id] + ":");
+                for (int j = 0; j < 5 && j < tempUser.Artists.Count; j++)
+                {
+                    if (!listeMedNewUSerArtists.Contains(tempUser.Artists.Skip(j).First().ThisArtist))
                     {
-                        try
-                        {
-                            Console.WriteLine($"TagID: {item.Id}\tTag Name: {Convert.GetTagName(item.Id)}");
-                        }
-                        catch (Exception)
-                        {
-                            Console.WriteLine($"TagID: {item.Id}\tTag Name: ERROR");
-                        }
+                        Console.WriteLine(tempUser.Artists.Skip(j).First().ThisArtist.Name);
                     }
                 }
-                Console.WriteLine("\nPress Any Key To Return");
-                Console.ReadKey();
-            }*/
+
+
+
+            }
+
+            Console.Read();
+
+
+            //Artist rec
+
+
+            /* while (true) { 
+             double prevCos = 0;
+             Artist hej = null;
+             Console.WriteLine(";");
+             int h = int.Parse(Console.ReadLine());
+
+             Artist jeh = Artists.ToList().Find(a => a.Value.Id == h).Value;
+                 Dictionary<int, double> rec = new Dictionary<int, double>(); 
+             double temp = 0;
+             foreach (var art in Artists)
+             {
+                 temp = pear.Calculate(jeh, art.Value);
+                     if (art.Value.Id == jeh.Id)
+                     {
+                         //
+                     }
+                     else
+                     {
+                         rec.Add(art.Value.Id, temp);
+                     }
+
+
+             }
+                 var nname = new ConvertIdName();
+             Console.WriteLine(pear.Calculate(jeh, jeh));
+             Console.WriteLine(jeh.Name);
+                 Console.WriteLine( );
+                var list = rec.OrderByDescending(art => art.Value);
+
+                 for (int i = 0; i < 5; i++)
+                 {
+                     Console.WriteLine(nname.ArtistFromID((list.Skip(i).First().Key), Artists).Name + " " + list.Skip(i).First().Value);
+                 }
+
+
+             }*/
+
+
+
+
+            //Keyword Seach
+
+            /*
+                        //Variables
+                        KeyWordSearch Search = new KeyWordSearch();
+                        ConvertIdName Convert = new ConvertIdName();
+                        List<Artist> DataArtist = Artists.ToList<Artist>();
+                        List<Tag> test = new List<Tag>();
+                        Artist BaseArtist = new Artist(0, "Default");
+                        BestMatch SingleBest = new BestMatch();
+                        double ProcentMatch = 0.0;
+                        Artist BestMatch = new Artist(0, "Default");
+
+                        while (true)
+                        {
+                            Console.Clear();
+                            //UserInput
+                            UserInput User = new UserInput();
+                            Console.Write("Enter An Artist Name or ID: ");
+                            while (!User.TryGetArtistFromUserInput(Console.ReadLine(), DataArtist, out BaseArtist))
+                            {
+                                Console.WriteLine("Cant Find The Artist You Are Looking For");
+                                Console.Write("Enter An Artist Name or ID: ");
+                            }
+                            if (!BaseArtist.TagIds.Any())
+                            {
+                                Console.WriteLine("This artist has no tag");
+                            }
+                            else
+                            {
+                                //Finding the best match
+                                FindBestMatch Match = new FindBestMatch();
+                                BestMatch = Match.BestMatch(DataArtist, BaseArtist, out ProcentMatch);
+                                Console.Clear();
+
+                                //Printing the result:
+                                Console.WriteLine($"Found The Best Match For: {BaseArtist.Id}: {BaseArtist.Name}");
+                                Console.WriteLine($"The best match is: {BestMatch.Id}: {BestMatch.Name}");
+                                Console.WriteLine($"The Best Match Has {ProcentMatch:0.00}% Tags In Commen With Base");
+                                Console.WriteLine("\n\nThe Commen Tags Are:");
+                                foreach (Tag item in Search.CompareTwoArtists(BestMatch, BaseArtist))
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine($"TagID: {item.Id}\tTag Name: {Convert.GetTagName(item.Id)}");
+                                    }
+                                    catch (Exception)
+                                    {
+                                        Console.WriteLine($"TagID: {item.Id}\tTag Name: ERROR");
+                                    }
+                                }
+                            }
+                            Console.WriteLine("\nPress Any Key To Return");
+                            Console.ReadKey();
+                        }*/
         }
     }
 
