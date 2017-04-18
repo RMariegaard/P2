ee8aad12815d6d6435b472b260970e27db3ebce1
Jackilans <mark1904@hotmail.dk>
2017-04-18 15:11:20 +0200

    Cleared Main and removed ToLasse

--- Program/Contentbased stuff/ConsoleApplication4/Program.cs
+++ Program/Contentbased stuff/ConsoleApplication4/Program.cs
@@ -11,61 +11,20 @@ namespace Recommender
     {
         static void Main(string[] args)
         {
-            /*************************************** Write file ***************************************/
-            Console.WriteLine("Reading text files, and writing binary files");
-            BinaryData Data = new BinaryData();
-            try
+            CreateRecommendations Recommender = new CreateRecommendations();
+            Console.WriteLine("Do you want to update the datafiles? (Y/N)");
+            bool readInput = (Console.ReadKey().KeyChar == 'Y')?true:false;
+            if (readInput)
             {
-                Data.DoItAll();
+                UpdateData.UpdateDataFiles();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Console.ReadKey();
-            }
-            Console.WriteLine("Done writing files");
-            /*************************************** The Program ***************************************/
-            string startupPath = Environment.CurrentDirectory;
-            //Does it twice to go back two folders
-            startupPath = Path.GetDirectoryName(startupPath);
-            startupPath = Path.GetDirectoryName(startupPath);
-            
-            Console.WriteLine("Reading File");
-            Dictionary<int, User> Users = BinarySerialization.ReadFromBinaryFile<Dictionary<int, User>>(startupPath + @"\DataFiles\users.bin");
-            Dictionary<int, Artist> Artists = BinarySerialization.ReadFromBinaryFile<Dictionary<int, Artist>>(startupPath + @"\DataFiles\artists.bin");
-            List<RoskildeArtist> RoskildeArtists = BinarySerialization.ReadFromBinaryFile<List<RoskildeArtist>>(startupPath + @"\DataFiles\Roskildeartists.bin");
-
-            Console.WriteLine("Done Reading File");
             Console.Clear();
-            var cosine = new Cosine();
-            int id = 0;
-            User newUser = new User(0);
-            Console.WriteLine();
+            /*************************************** The Program ***************************************/
+            Recommender.LoadFiles();
+
             while (true)
             {
-               Console.WriteLine("ID: ");
-                while (!int.TryParse(Console.ReadLine(), out id));
-
-                if(Users.ContainsKey(id))
-                    newUser = Users[id];
-
-                StringBuilder streng = new StringBuilder();
-
-                var recommendedArtists = CollaborativeFiltering.RecommendArtists(newUser, Users, RoskildeArtists);// ContentBasedFiltering.RecommedArtists(cosine.GetCosine, newUser, RoskildeArtists, 10); 
-                streng.AppendLine("Collarborative");
-                recommendedArtists.OrderByDescending(x => x.Value.CollaborativeFilteringRating).ToList().ForEach(x => streng.AppendLine(x.Value.thisArtist.Name + " - " + x.Value.CollaborativeFilteringRating));
-
-                var recommendedArtists2 = ContentBasedFiltering.RecommedArtists(cosine.GetCosine, newUser, RoskildeArtists, 10);
-                streng.AppendLine("\nContentBasedFiltering: ");
-                recommendedArtists2.OrderByDescending(x => x.Value.ContentBasedFilteringRating).ToList().ForEach(x => streng.AppendLine(x.Value.thisArtist.Name + " - " + x.Value.ContentBasedFilteringRating));
-                
-
-                                    streng.AppendLine(" ---------");
-                    newUser.Artists.OrderByDescending(x => x.Value.Weight).ToList().ForEach(x => streng.AppendLine(x.Value.ThisArtist.Name + " - " + x.Value.Weight));
-                    Console.Read();
-                File.AppendAllText(startupPath + @"\tilLasse.txt", streng.ToString());
-                    Console.Clear();
-                
+                Recommender.Recommender();
             }
         }
     }
---
+++ Program/Contentbased stuff/ConsoleApplication4/UpdateData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recommender
+{
+    public class UpdateData
+    {
+        public static void UpdateDataFiles()
+        {
+            /*************************************** Write file ***************************************/
+            Console.WriteLine("Reading text files, and writing binary files");
+            BinaryData Data = new BinaryData();
+            try
+            {
+                Data.DoItAll();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
+            Console.WriteLine("Done writing files");
+        }
+    }
+}
