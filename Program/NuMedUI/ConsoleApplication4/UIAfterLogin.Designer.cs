b69285ccc5b9b4f59f81f90b166a8b7825c0826f
lsvenn <lsvenn16@student.aau.dk>
2017-04-19 15:21:33 +0100

    UI ting
    håber det virker...

--- Program/NuMedUI/ConsoleApplication4/UIAfterLogin.Designer.cs
+++ Program/NuMedUI/ConsoleApplication4/UIAfterLogin.Designer.cs
@@ -29,13 +29,16 @@
         private void InitializeComponent()
         {
             this.GreetingLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.HardAddButton = new System.Windows.Forms.Button();
             this.RoskildeArtistsList = new System.Windows.Forms.CheckedListBox();
+            this.HardAddButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // GreetingLabel
             // 
+            this.GreetingLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.GreetingLabel.AutoSize = true;
             this.GreetingLabel.Location = new System.Drawing.Point(13, 13);
             this.GreetingLabel.Name = "GreetingLabel";
@@ -43,47 +46,53 @@
             this.GreetingLabel.TabIndex = 0;
             this.GreetingLabel.Text = "Welcome to the user!";
             // 
-            // button1
+            // RoskildeArtistsList
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(12, 978);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(680, 63);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Find Recommendations";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.RoskildeArtistsList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RoskildeArtistsList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RoskildeArtistsList.FormattingEnabled = true;
+            this.RoskildeArtistsList.Location = new System.Drawing.Point(12, 31);
+            this.RoskildeArtistsList.Name = "RoskildeArtistsList";
+            this.RoskildeArtistsList.Size = new System.Drawing.Size(387, 540);
+            this.RoskildeArtistsList.TabIndex = 10;
+            this.RoskildeArtistsList.ThreeDCheckBoxes = true;
             // 
             // HardAddButton
             // 
-            this.HardAddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.HardAddButton.Location = new System.Drawing.Point(12, 910);
+            this.HardAddButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.HardAddButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.HardAddButton.Location = new System.Drawing.Point(0, 571);
             this.HardAddButton.Name = "HardAddButton";
-            this.HardAddButton.Size = new System.Drawing.Size(680, 62);
-            this.HardAddButton.TabIndex = 6;
+            this.HardAddButton.Size = new System.Drawing.Size(411, 62);
+            this.HardAddButton.TabIndex = 9;
             this.HardAddButton.Text = "Add to list";
             this.HardAddButton.UseVisualStyleBackColor = true;
-            this.HardAddButton.Click += new System.EventHandler(this.HardAddButton_Click);
             // 
-            // RoskildeArtistsList
+            // button1
             // 
-            this.RoskildeArtistsList.FormattingEnabled = true;
-            this.RoskildeArtistsList.Location = new System.Drawing.Point(12, 29);
-            this.RoskildeArtistsList.Name = "RoskildeArtistsList";
-            this.RoskildeArtistsList.Size = new System.Drawing.Size(680, 874);
-            this.RoskildeArtistsList.TabIndex = 7;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 633);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(411, 62);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Find Recommendations";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // UIAfterLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 1053);
+            this.ClientSize = new System.Drawing.Size(411, 695);
             this.Controls.Add(this.RoskildeArtistsList);
             this.Controls.Add(this.HardAddButton);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.GreetingLabel);
             this.Name = "UIAfterLogin";
             this.Text = "UIAfterLogin";
+            this.Load += new System.EventHandler(this.UIAfterLogin_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,8 +101,8 @@
         #endregion
 
         private System.Windows.Forms.Label GreetingLabel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button HardAddButton;
         private System.Windows.Forms.CheckedListBox RoskildeArtistsList;
+        private System.Windows.Forms.Button HardAddButton;
+        private System.Windows.Forms.Button button1;
     }
 }
