511a91c4a0d73b332c28d713656214d5e8df54e2
Jackilans <mark1904@hotmail.dk>
2017-04-19 11:20:33 +0200

    Lige ændret main så det også virker med lille y, happy?

--- Program/Contentbased stuff/ConsoleApplication4/Program.cs
+++ Program/Contentbased stuff/ConsoleApplication4/Program.cs
@@ -13,7 +13,7 @@ namespace Recommender
         {
             CreateRecommendations Recommender = new CreateRecommendations();
             Console.WriteLine("Do you want to update the datafiles? (Y/N)");
-            bool readInput = (Console.ReadKey().KeyChar == 'Y')?true:false;
+            bool readInput = (Char.ToUpper(Console.ReadKey().KeyChar) == 'Y')?true:false;
             if (readInput)
             {
                 UpdateData.UpdateDataFiles();
