5c79dfe916c9ee20733dea3083fae8e7c1e70992
Caspersn2 <caspersn2@gmail.com>
2017-04-25 11:13:48 +0200

    Farve, resize billeder, andet småt

--- Program/NuMedUI/ConsoleApplication4/UiScheduele.cs
+++ Program/NuMedUI/ConsoleApplication4/UiScheduele.cs
@@ -76,16 +76,6 @@ namespace Recommender
                 button.Width += 20;
                 left += button.Width + 2;
             }
-
-            //Adding to the Scheduele window
-            /* int i = 0;
-             foreach (RecommendGUI test in GUIList.OrderBy(x => x.TimeOfConcertLabel.Text).ToList())
-             {
-                 test.calcLocation(new Point(5,105*i), new Size(400, 100));
-                 panel1.Controls.Add(test.Background);
-                 i++;
-             }*/
-
         }
 
         private void NewButton_Click(object sender, EventArgs e)
@@ -99,7 +89,7 @@ namespace Recommender
             foreach (RecommendGUI item in GUIList.Where(x => x.artist.TimeOfConcert.Date.Day.ToString() == btn.Tag.ToString()).OrderBy(x => x.TimeOfConcertLabel.ToString()))
             {
                 item.calcLocation(new Point(5, 105 * counter), new Size(400, 100));
-                panel1.Controls.Add(item.Background);
+                panel1.Controls.Add(item.Element);
                 counter++;
 
             }
@@ -113,7 +103,7 @@ namespace Recommender
         public Label RatingLabel;
         public Label TimeOfConcertLabel;
         public Label Scene;
-        public Panel Background;
+        public Panel Element;
         public Color color;
         public Rectangle Rect;
         public string RatingFrom;
@@ -123,7 +113,7 @@ namespace Recommender
         {
             this.artist = artist;
             this.RatingFrom = RatingFrom;
-            Background = new Panel();
+            Element = new Panel();
             Picture = new PictureBox();
             NameLabel = new Label();
             RatingLabel = new Label();
@@ -131,34 +121,38 @@ namespace Recommender
             Scene = new Label();
             color = new Color();
 
+            //Creating color
             if (RatingFrom == "Collab")
             {
                 RatingLabel.Text = $"Collaborative: {artist.CollaborativeFilteringRating}";
-                if ((int)((255 / 10) * artist.CollaborativeFilteringRating) > 255)
-                    color = Color.FromArgb(255, 255, 255);
-                else
-                    color = Color.FromArgb(255, (int)((255 / 10) * artist.CollaborativeFilteringRating), (int)((255 / 10) * artist.CollaborativeFilteringRating));
+                if (artist.CollaborativeFilteringRating > 5)
+                {
+                    color = Color.FromArgb(238, 113, 3);
+                }
             }
             else if (RatingFrom == "Content")
             {
                 RatingLabel.Text = $"ContentBased: { artist.ContentBasedFilteringRating}";
-                color = Color.FromArgb(255, (int)(255 * artist.ContentBasedFilteringRating), (int)(255 * artist.ContentBasedFilteringRating));
+                if (artist.ContentBasedFilteringRating > 0.5)
+                {
+                    color = Color.FromArgb(238, 113, 3);
+                }
             }
             else if (RatingFrom == "HardAdd")
             {
                 RatingLabel.Text = "You have added this";
-                color = Color.FromArgb(255, 255, 255);
+                color = Color.FromArgb(238, 113, 3);
             }
             
-
+            //Creating the visual
             string startupPath = Environment.CurrentDirectory;
             startupPath = Path.GetDirectoryName(startupPath);
             startupPath = Path.GetDirectoryName(startupPath);
 
-            Background.BackColor = color;
+            Element.BackColor = color;
             try
             {
-                Picture.Image = Image.FromFile(startupPath + $"\\pics\\{artist.Name.ToUpper()}.png");
+                Picture.Image = ResizeBitmap.ResizeImage(Image.FromFile(startupPath + $"\\pics\\{artist.Name.ToUpper()}.png"), 100, 100);
             }
             catch (FileNotFoundException)
             {
@@ -172,20 +166,18 @@ namespace Recommender
             RatingLabel.AutoSize = true;
             TimeOfConcertLabel.Visible = true;
             TimeOfConcertLabel.AutoSize = true;
-
-
+            
             Scene.Text = artist.Scene;
             NameLabel.Text = artist.Name;
-
-            TimeOfConcertLabel.Text = artist.TimeOfConcert.TimeOfDay.ToString();
             
+            TimeOfConcertLabel.Text = artist.TimeOfConcert.TimeOfDay.ToString();
         }
         
         public void calcLocation(Point TopLeft, Size size)
         {
-            Background.Location = TopLeft;
-            Background.Size = size;
-            Background.BackColor = color;
+            Element.Location = TopLeft;
+            Element.Size = size;
+            Element.BackColor = color;
 
             Rect = new Rectangle(TopLeft, size);
             Picture.Location = new Point(0, 0);
@@ -195,10 +187,10 @@ namespace Recommender
             RatingLabel.Location = new Point(size.Height + 2, 25);
             TimeOfConcertLabel.Location = new Point(size.Height + 2, 45);
 
-            Background.Controls.Add(Picture);
-            Background.Controls.Add(NameLabel);
-            Background.Controls.Add(RatingLabel);
-            Background.Controls.Add(TimeOfConcertLabel);
+            Element.Controls.Add(Picture);
+            Element.Controls.Add(NameLabel);
+            Element.Controls.Add(RatingLabel);
+            Element.Controls.Add(TimeOfConcertLabel);
 
         }
     }
