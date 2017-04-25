b1b5a8a1d98219b013b538763e9bfd3599cfd7c7
casper-lp <cpede16@student.aau.dk>
2017-04-25 14:22:00 +0200

    4 patter skal den ha!

--- Program/NuMedUI/ConsoleApplication4/Program.cs
+++ Program/NuMedUI/ConsoleApplication4/Program.cs
@@ -12,26 +12,11 @@ namespace Recommender
     {
         static void Main(string[] args)
         {
-            //CreateRecommendations Recommender = new CreateRecommendations();
-            /*
-            Console.WriteLine("Do you want to update the datafiles? (Y/N)");
-            bool readInput = (Char.ToUpper(Console.ReadKey().KeyChar) == 'Y') ? true : false;
-            if (readInput)
-            {
-                UpdateData.UpdateDataFiles();
-            }
-            Console.Clear();
             /*************************************** The Program ***************************************/
-            //Recommender.LoadFiles();
             Application.EnableVisualStyles();
             Application.Run(new UI());
 
-            /*
-            while (true)
-            {
-                Recommender.Recommender();
-            }
-            */
+            /* 4 (.Y.) skal den ha'! */
         }
     }
 }
