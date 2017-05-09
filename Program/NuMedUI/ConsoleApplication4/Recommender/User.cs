d83fd29b53f7cb26272b24eb00d5ca5bc30695f0
lsvenn <lsvenn16@student.aau.dk>
2017-05-09 13:25:05 +0100

    ændret i user med kommentarer
    ændret i user med kommentarer

--- Program/NuMedUI/ConsoleApplication4/Recommender/User.cs
+++ Program/NuMedUI/ConsoleApplication4/Recommender/User.cs
@@ -9,26 +9,25 @@ namespace Recommender
     [Serializable]
     public class User : ITaggable
     {
+        // The id of the user is stored here:
         private int _id;
         public int Id
         {
             get { return _id; }
             private set { _id = value; }
         }
-
+        // Dictionaries for Artists and Tags:
+        // Artist dictionay is filled while loading the data and making the user:
         public Dictionary<int, Userartist> Artists { get; private set; }
+        // Tags dictionary will be filled by using the method UserTagHandling(), which will be called when making the data:
         public Dictionary<int, Tag> Tags { get; private set; }
-        private double _total_tag_amount;
-        private double _totalt_listen_amount;
-        private double _averageContentRating;
-        private double _averageColabRating;
-        public double AverageContentRating { get { return _averageContentRating; } }
-        public double AverageColabRating { get { return _averageColabRating; } }
 
+        private double _totalTagAmount;
+        public double TotalTagAmount { get { return _totalTagAmount; } }
+        private double _totaltListenAmount;
+        public double TotalListenAmount { get { return _totaltListenAmount; } }
 
-        public double TotalTagAmount { get { return _total_tag_amount; } }
-        public double TotalListenAmount { get { return _totalt_listen_amount; } }
-
+        // Constructer:
         public User(int id)
         {
             Artists = new Dictionary<int, Userartist>();
@@ -36,38 +35,28 @@ namespace Recommender
             Id = id; 
         }
 
-        //To testing
-        public User(int id, Dictionary<int, Tag> tags)
+        // Method that makes the TagsDictionary, relative to the Artists this user has heard:
+        public void UserTagHandling()
         {
-            Tags = tags;
-            Id = id;
-        }
-
-        //Same
-        public User(int id, Dictionary<int, Userartist> artists)
-        {
-            Artists = artists;
-            Id = id;
-        }
-
-        // Makes the users tag list depending on the users its heard, their top 5 tags, and how much the user has heard theese artists:
-        public void TagCalc()
-        {
+            // Itterating though all artists in the users ArtistDictionary:
             foreach(KeyValuePair<int, Userartist> artist in Artists)
             {
+                // And array of tags is made of the tags the current artist has:
                 Tag[] ArrayOfTags = artist.Value.ThisArtist.Tags.Values.OrderByDescending(p => p.Amount).ToArray();
 
-                //Set limit to numbers of tags if it is below 10
+                //Set limit to numbers of tags if it is below 10, so its only the top 10 most used tag for an artist that is used:
                 int limit = 10 > ArrayOfTags.Count() ? ArrayOfTags.Count() : 10; 
 
                 for (int i = 0; i < limit; i++)
                 {
                     int currentTagID = ArrayOfTags[i].Id;
+                    // If this tag is already represented in this Users TagDictionary, then the Tag.Amount will be increased by 
+                    // the amount of listens the User has on the artist, and the weight of that Tag on the artist:
                     if (Tags.ContainsKey(currentTagID))
                     {
                         Tags[currentTagID].Amount += artist.Value.Amount * artist.Value.ThisArtist.Tags[currentTagID].Weight; 
                     }
-
+                    // If its a new tag a new tag is added:
                     else
                     {
                         Tag Temptag = new Tag(currentTagID);
@@ -76,42 +65,26 @@ namespace Recommender
                     }
                 }
             }
-
+            // After the Tag Dictionay is made, the total tag amount, and the weight of each individual tags will be calculated:
             foreach(var tag in Tags)
             {
-                _total_tag_amount += tag.Value.Amount;
+                _totalTagAmount += tag.Value.Amount;
             }
             foreach (var tag in Tags)
             {
-                tag.Value.Weight = (100 / _total_tag_amount) * tag.Value.Amount;
+                tag.Value.Weight = (100 / _totalTagAmount) * tag.Value.Amount;
             }
         } 
 
+        // Calculates the percentage an artist has been heard by this user, based on all the artists the user has heard:
         public void CalculateArtistWeight()
         {
             foreach(var artist in Artists)
             {
-                _totalt_listen_amount += artist.Value.Amount;
+                _totaltListenAmount += artist.Value.Amount;
             }
             foreach(var artist in Artists)
             {
-                artist.Value.Weight = (artist.Value.Amount / _totalt_listen_amount) * 100;
+                artist.Value.Weight = (artist.Value.Amount / _totaltListenAmount) * 100;
             }
         }
-
-        // After Recommendations are made, the users average rating value is calculated for deciding which recommendation to choose in a possible conflict.
-        public void CalculateAverageOfUserRatings(List<RecommendedArtist> listOfRecommendations)
-        {
-            double totalColabRating = 0;
-            double totalContentRating = 0;
-            foreach (RecommendedArtist artist in listOfRecommendations)
-            {
-                totalColabRating += artist.CollaborativeFilteringRating;
-                totalContentRating += artist.ContentBasedFilteringRating;
-
-            }
-            _averageColabRating = (100 / totalColabRating) * listOfRecommendations.Count;
-            _averageContentRating = (100 / totalContentRating) * listOfRecommendations.Count;
-        }
-    }
-}
