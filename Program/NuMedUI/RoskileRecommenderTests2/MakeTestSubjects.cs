849ec77fd9e95ac3728f100d143dc20ae23bd046
lsvenn <lsvenn16@student.aau.dk>
2017-05-11 13:50:38 +0100

    tilføjede User test
    tilføjede User test

---
+++ Program/NuMedUI/RoskileRecommenderTests2/MakeTestSubjects.cs
@@ -0,0 +1,99 @@
+﻿using Recommender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoskileRecommenderTests2
+{
+    // This is a class containing methods to make testsubjects for UnitTesting.
+    class MakeTestSubjects
+    {
+        // Method to make Users with dictionaries of Userartists containing ID:
+        public User MakeSimpleUserArtistTestSubject(int ID, int[] keys)
+        {
+            int count = 0;
+            User testUser = new User(ID);
+
+            foreach (int key in keys)
+            {
+                testUser.Artists.Add(key, new Userartist(key, 0, null));
+            }
+
+            return testUser;
+        }
+
+        // Method to make Users with dictionaries of Userartists containing ID, weight and amount values:
+        public User MakeUserWithArtistWeightAndAmountOnlyTestSubject(int ID, int[] artistKeys, double[] weightValues, int[] amountValues)
+        {
+            if (artistKeys.Length != weightValues.Length || weightValues.Length != amountValues.Length)
+            {
+                throw new Exception("Error when making users (MakeUserWithWeightAndAmountOnlyArtistsTestSubject) for test! All arrays of data most be same length!");
+            }
+            int count = 0;
+            User testUser = new User(ID);
+
+            foreach (int key in artistKeys)
+            {
+                testUser.Artists.Add(key, new Userartist(key, amountValues[count], null));
+                testUser.Artists[key].Weight = weightValues[count];
+            }
+
+            return testUser;
+        }
+
+        // Method for making Users with a dictionary of Userartist containing ID, weight, amount and has a reference to an existing artist:
+        public User MakeUserWithArtistsWithTagsTestSubject(int ID, int[] keys, double[] weightValues, int[] amountValues, Artist[] artists)
+        {
+            // If methods is called with empty artist array, default artists will be made:
+            if (artists == null || artists.Length == 0)
+            {
+                int limit = keys.Length;
+                artists = new Artist[limit];
+                for (int i = 0; i < limit; i++)
+                {
+                    artists[i] = MakeSimpleArtistTestSubject(i);
+                }
+            }
+            // Check if method is called correct:
+            if (keys.Length != weightValues.Length || weightValues.Length != amountValues.Length ||
+                amountValues.Length != artists.Length)
+            {
+                throw new Exception("Error when making users (MakeUserWithArtists) for test! All arrays of data most be same length!");
+            }
+
+            int count = 0;
+            User testUser = new User(ID);
+
+            foreach (int key in keys)
+            {
+                testUser.Artists.Add(key, new Userartist(key, amountValues[count], artists[count]));
+                testUser.Artists[key].Weight = weightValues[count];
+            }
+
+            return testUser;
+        }
+
+        // Method for making simple artists containing a default name and empty tag dictionary:
+        public Artist MakeSimpleArtistTestSubject(int ID)
+        {
+            return new Artist(ID, null, "test");
+        }
+
+        // Method for making artists containing Tagdata.
+        public Artist MakeArtistWithTagsTestSubject(int ID, int[] tagAmounts)
+        {
+            Dictionary<int, Tag> tags = new Dictionary<int, Tag>();
+            int tagID = 0;
+            foreach (int tagAmount in tagAmounts)
+            {
+                tagID++;
+                tags.Add(tagID, new Tag(tagID));
+                tags[tagID].Amount = tagAmount;
+            }
+            Artist testArtist = new Artist(ID, tags, "test");
+            return testArtist;
+        }
+    }
+}
---
+++ Program/NuMedUI/RoskileRecommenderTests2/Recommender/UserTests.cs
@@ -0,0 +1,149 @@
+﻿using NUnit.Framework;
+using Recommender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using RoskileRecommenderTests2;
+
+namespace Recommender.Tests
+{
+    [TestFixture()]
+    public class UserTests
+    {
+        MakeTestSubjects makeTestSubjects = new MakeTestSubjects();
+        // TestCases for User ID. Not much to test here, other than it is possible to assign all kinds of integers as ID´s. 
+        // ID´s are loaded from the dataset, and no real validation is made withing the class, only while loading the data.
+        [TestCase(-100000000)]
+        [TestCase(-100)]
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(100)]
+        [TestCase(100000000)]
+        public void UserIDTest(int userID)
+        {
+            User testUser = new User(userID);
+            Assert.AreEqual(userID, testUser.Id);
+        }
+
+        // These test will check if the UserArtist dictionary works as intended:
+        // Test that a given amount of artist is actually added to the Users artist dictionary:
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(50)] // Most users have 50 userartist
+        [TestCase(10000)]
+        public void UserArtistDictHasCorrectAmountTest(int amountOfArtists)
+        {
+            User testUser = new User(1);
+
+            for(int i = 0; i < amountOfArtists; i++)
+            {
+                testUser.Artists.Add(i, new Userartist(i, 0, null));
+            }
+            Assert.AreEqual(testUser.Artists.Count, amountOfArtists);
+        }
+
+        // This test will test if userartists amount correct Value:
+        [TestCase(0, 1, 0)]
+        [TestCase(0, 1, 1)]
+        [TestCase(0, 1, 10)]
+        [TestCase(0, 1, 1378)]
+        [TestCase(0, 1, 1234456)]
+        [TestCase(0, 50, 0)]
+        [TestCase(0, 50, 1)]
+        [TestCase(0, 50, 10)]
+        [TestCase(0, 50, 1378)]
+        [TestCase(0, 50, 1234456)]
+        [TestCase(1, 2, 0)]
+        [TestCase(1, 2, 1)]
+        [TestCase(1, 2, 10)]
+        [TestCase(1, 2, 1378)]
+        [TestCase(1, 2, 1234456)]
+        [TestCase(1, 50, 0)]
+        [TestCase(1, 50, 1)]
+        [TestCase(1, 50, 10)]
+        [TestCase(1, 50, 1378)]
+        [TestCase(1, 50, 1234456)]
+        [TestCase(49, 50, 0)]
+        [TestCase(49, 50, 1)]
+        [TestCase(49, 50, 10)]
+        [TestCase(49, 50, 1378)]
+        [TestCase(49, 50, 1234456)]
+        [TestCase(0, 1749, 0)]
+        [TestCase(49, 1749, 1)]
+        [TestCase(799, 1749, 10)]
+        [TestCase(1748, 1749, 1378)]
+        [TestCase(1748, 1749, 1234456)]
+        public void UserArtistDictContainsCorrectAmountTest(int ID, int amountOfArtists, int amount)
+        {
+            User testUser = new User(1);
+
+            for (int i = 0; i < amountOfArtists; i++)
+            {
+                if (i == ID)
+                {
+                    testUser.Artists.Add(i, new Userartist(i, amount, new Artist(i, null, "Test")));
+                }
+                testUser.Artists.Add(i, new Userartist(i, 0, new Artist(i, null, "Test")));
+            }
+            Assert.AreEqual(testUser.Artists[ID].Amount, amount);
+        }
+
+        // This test will test if userartists reference to artist is the correct ID/key:
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(1, 50)]
+        [TestCase(1, 1000)]
+        [TestCase(2, 2)]
+        [TestCase(50, 50)]
+        [TestCase(1234, 1234)]
+        [TestCase(17, 50)]
+        [TestCase(142, 1000)]
+        public void UserArtistDictContainsCorrectValuesIDTest(int ID, int amountOfArtists)
+        {
+            User testUser = new User(1);
+
+            for (int i = 0; i < amountOfArtists; i++)
+            {
+                testUser.Artists.Add(i, new Userartist(i, 0, new Artist(i, null, "Test")));
+            }
+            Assert.AreEqual(testUser.Artists[ID].Id, ID);
+        }
+
+
+
+        [Test()]
+        public void UserTagHandlingTest()
+        {
+            Assert.Fail();
+        }
+
+        // Test that the method CalculateArtistWeight() calculates correct weight value:
+        [TestCase(0, 1, new int[] { 1 }, 100)]
+        [TestCase(0, 1, new int[] { 100 }, 100)]
+        [TestCase(0, 1, new int[] { 100000 }, 100)]
+        [TestCase(0, 2, new int[] { 1, 1 }, 50)]
+        [TestCase(1, 2, new int[] { 1, 1 }, 50)]
+        public void CalculateArtistWeightTest(int ID, int amountOfArtists, int[] artistsAmounts, double expectedWeight)
+        {
+            User testUser = new User(1);
+
+            for (int i = 0; i < amountOfArtists; i++)
+            {
+                testUser.Artists.Add(i, new Userartist(i, artistsAmounts[i], new Artist(i, null, "Test")));
+            }
+            testUser.CalculateArtistWeight();
+
+            Assert.AreEqual(expectedWeight, testUser.Artists[ID].Weight);
+        }
+
+       
+
+
+
+    }
+}
